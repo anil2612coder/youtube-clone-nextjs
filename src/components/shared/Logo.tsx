6b55c428d21323d4a312b2373400da801c3c4d1b
Anil <ak26122k@gmail.com>
2024-01-03 06:48:08 +0530

    prisma and create model

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -9,6 +9,7 @@ const Logo = () => {
   return (
     <Link href="/">
         <Image src="/images/logo.png" alt="logo" className=" cursor-pointer  " height={50} width={120}/>
+        {/* <img src="/images/logo.png" alt="logo" className="cursor-pointer w-46 h-20" /> */}
     </Link>
   )
 }
